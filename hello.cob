1b214d3d0c91bff822764492151b532055687624
gonzaemon111 <goki14914@icloud.com>
2019-09-21 04:36:33 +0900

    init commit

---
+++ hello.cob
@@ -0,0 +1,13 @@
+       identification division.
+       program-id. hello.
+
+       environment division.
+
+       data division.
+
+       procedure division.
+
+       hello.
+        display "HELLO"
+        stop run.
+
